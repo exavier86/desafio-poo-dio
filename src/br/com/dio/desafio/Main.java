542c45c0d931d5fd06d4e45e04f7e5023b540e55
Eriton Xavier <ertao1@hotmail.com>
2022-03-15 15:47:51 -0300

    Inscrevendo devs no Bootcamp

--- src/br/com/dio/desafio/Main.java
+++ src/br/com/dio/desafio/Main.java
@@ -2,7 +2,9 @@ package br.com.dio.desafio;
 
 import java.time.LocalDate;
 
+import br.com.dio.desafio.dominio.Bootcamp;
 import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Dev;
 import br.com.dio.desafio.dominio.Mentoria;
 
 public class Main {
@@ -23,10 +25,38 @@ public class Main {
         mentoria.setTitulo("mentoria de java");
         mentoria.setDescricao("descrição mentoria java");
         mentoria.setData(LocalDate.now());
+		
+		Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descrição Bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
 
-        System.out.println(curso1);
-        System.out.println(curso2);
-        System.out.println(mentoria);
+        Dev devEriton = new Dev();
+        devEriton.setNome("Eriton");
+        devEriton.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos Eriton:" + devEriton.getConteudosInscritos());
+        devEriton.progredir();
+        devEriton.progredir();
+        System.out.println("-");
+        System.out.println("Conteúdos Inscritos Eriton:" + devEriton.getConteudosInscritos());
+        System.out.println("Conteúdos Concluídos Eriton:" + devEriton.getConteudosConcluidos());
+        System.out.println("XP:" + devEriton.calcularTotalXp());
+
+        System.out.println("-------");
+
+        Dev devMalu = new Dev();
+        devMalu.setNome("Malu");
+        devMalu.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos Malu:" + devMalu.getConteudosInscritos());
+        devMalu.progredir();
+        devMalu.progredir();
+        devMalu.progredir();
+        System.out.println("-");
+        System.out.println("Conteúdos Inscritos Malu:" + devMalu.getConteudosInscritos());
+        System.out.println("Conteúdos Concluidos Malu:" + devMalu.getConteudosConcluidos());
+        System.out.println("XP:" + devMalu.calcularTotalXp());
 
 	}
 }
